e31690077fe27cdac6e8cdfcf2bb0de121736877
Shamibei <shannel.mibei@student.moringaschool.com>
2021-09-06 09:23:52 +0300

    addition of names

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-const generateAkan=function(){
+const generateAkan=function(e){
+    e.preventDefault();
+
+    const theFemaleNames=["Akosua","Adowa","Abenaa","Akua","Yaa","Afua","Ama"];
+
+    const theMaleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+
     
 }
