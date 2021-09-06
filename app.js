281b75b5cddba4ae412ee5bc4b3469502549bee5
Shamibei <shannel.mibei@student.moringaschool.com>
2021-09-06 10:59:15 +0300

    constants

--- app.js
+++ app.js
@@ -5,5 +5,18 @@ const generateAkan=function(e){
 
     const theMaleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 
-    
+    const form = document.querySelector("#form")
+
+    const userName = document.getElementById("name");
+
+    const date = document.getElementById("date");
+
+    const month = document.getElementById("month")
+
+    const year = document.getElementById("year")
+
+    const genderFemale = document.getElementById("female")
+
+    const genderFemale = document.getElementById("male")
+
 }
